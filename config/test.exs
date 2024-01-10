b448706ed39b573aa338556a00804c0cc28cbb5d
bchamagne <bastien@archethic.net>
2024-01-10 12:00:50 +0100

    set a random port for test env, to be able to test when a server is running

--- config/test.exs
+++ config/test.exs
@@ -4,3 +4,6 @@ config :archethic_fas, ArchethicFAS.Quotes.Provider.CoinMarketCap,
   key: "b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c",
   endpoint: "sandbox-api.coinmarketcap.com",
   transport_opts: [verify: :verify_none]
+
+config :archethic_fas,
+  api_port: 33333
