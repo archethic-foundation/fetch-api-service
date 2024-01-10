eef1515e29a49113af5a25645109519fdcf07833
bchamagne <bastien@archethic.net>
2024-01-10 12:02:41 +0100

    better error when no currency

--- lib/archethic_fas/route/v1/quotes_latest.ex
+++ lib/archethic_fas/route/v1/quotes_latest.ex
@@ -13,18 +13,17 @@ defmodule ArchethicFAS.Route.V1.QuotesLatest do
   def call(conn, _opts) do
     conn = fetch_query_params(conn)
 
-    currencies_str =
-      conn.query_params
-      |> Map.get("currency", "")
-      |> String.split(",")
-      |> Enum.map(&String.trim/1)
-
-    with {:ok, currencies} <- Currency.cast_many(currencies_str),
+    with {:ok, currencies_str} <- extract_currencies_from_query_string(conn),
+         {:ok, currencies} <- Currency.cast_many(currencies_str),
          {:ok, quotes} <- Quotes.get_latest(currencies) do
       conn
       |> put_resp_content_type("application/json")
       |> send_resp(200, Jason.encode!(quotes))
     else
+      {:error, :no_currency_specified} ->
+        conn
+        |> send_resp(400, "Bad request: missing currency parameter")
+
       {:error, {:invalid_currency, currency}} ->
         conn
         |> send_resp(400, "Bad request: invalid currency: #{currency}")
@@ -34,6 +33,16 @@ defmodule ArchethicFAS.Route.V1.QuotesLatest do
 
         conn
         |> send_resp(500, "Internal error")
+    end
+  end
+
+  defp extract_currencies_from_query_string(conn) do
+    case conn.query_params
+         |> Map.get("currency", "")
+         |> String.split(",")
+         |> Enum.map(&String.trim/1) do
+      [""] -> {:error, :no_currency_specified}
+      strs -> {:ok, strs}
     end
   end
 end
