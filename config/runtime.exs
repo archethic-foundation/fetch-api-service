1d21cf9d57e6eace6998ba3ba7a367d2b503f1a2
bchamagne <bastien@archethic.net>
2024-01-18 09:59:28 +0100

    remove runtime conf because it's also used in test

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,12 +1,1 @@
 import Config
-
-IO.puts("==========================")
-IO.puts("== APPLYING RUNTIME CONFIG ==")
-IO.puts("==========================")
-
-config :archethic_fas, ArchethicFAS.Quotes.Provider.CoinMarketCap,
-  key: System.get_env("ARCHETHIC_CMC_PRO_API_KEY"),
-  endpoint: "pro-api.coinmarketcap.com"
-
-config :archethic_fas,
-  api_port: System.get_env("ARCHETHIC_FAS_PORT", "3000") |> String.to_integer()
