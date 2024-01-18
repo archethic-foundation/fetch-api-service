e4e9e10ea7e1656df74e686e7eb161088a117949
bchamagne <bastien@archethic.net>
2024-01-18 09:33:37 +0100

    copy/paste prod conf into runtime conf

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,5 +1,12 @@
 import Config
 
-# this config is loaded when in release mode
+IO.puts("==========================")
+IO.puts("== APPLYING RUNTIME CONFIG ==")
+IO.puts("==========================")
 
-import_config("prod.exs")
+config :archethic_fas, ArchethicFAS.Quotes.Provider.CoinMarketCap,
+  key: System.get_env("ARCHETHIC_CMC_PRO_API_KEY"),
+  endpoint: "pro-api.coinmarketcap.com"
+
+config :archethic_fas,
+  api_port: System.get_env("ARCHETHIC_FAS_PORT", "3000") |> String.to_integer()
