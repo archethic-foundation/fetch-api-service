bdd414a449a731e419cbb877b10379f43350b94b
bchamagne <bastien@archethic.net>
2024-01-18 09:22:49 +0100

    remove fetch_env because it require the env to be available on compilation

--- config/prod.exs
+++ config/prod.exs
@@ -5,5 +5,5 @@ IO.puts("== RUNNING IN PROD MODE ==")
 IO.puts("==========================")
 
 config :archethic_fas, ArchethicFAS.Quotes.Provider.CoinMarketCap,
-  key: System.fetch_env!("ARCHETHIC_CMC_PRO_API_KEY"),
+  key: System.get_env("ARCHETHIC_CMC_PRO_API_KEY"),
   endpoint: "pro-api.coinmarketcap.com"
