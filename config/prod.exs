fdd2991efc00d92712f8fa3ecbcdeed71746d454
Neylix <julien.leclerc05@protonmail.com>
2025-04-01 12:16:52 +0200

    Update runtime config

--- config/prod.exs
+++ config/prod.exs
@@ -5,11 +5,4 @@ IO.puts("== RUNNING IN PROD MODE ==")
 IO.puts("==========================")
 
 config :archethic_fas, ArchethicFAS.QuotesLatest.Provider.CoinMarketCap,
-  key: System.get_env("ARCHETHIC_CMC_PRO_API_KEY"),
   endpoint: "pro-api.coinmarketcap.com"
-
-config :archethic_fas, ArchethicFAS.QuotesHistorical.Provider.Coingecko,
-  key: System.get_env("ARCHETHIC_COINGECKO_API_KEY")
-
-config :archethic_fas,
-  api_port: System.get_env("ARCHETHIC_FAS_PORT", "3000") |> String.to_integer()
--- config/runtime.exs
+++ config/runtime.exs
@@ -1,1 +1,12 @@
 import Config
+
+if config_env() == :prod do
+  config :archethic_fas, ArchethicFAS.QuotesLatest.Provider.CoinMarketCap,
+    key: System.get_env("ARCHETHIC_CMC_PRO_API_KEY")
+
+  config :archethic_fas, ArchethicFAS.QuotesHistorical.Provider.Coingecko,
+    key: System.get_env("ARCHETHIC_COINGECKO_API_KEY")
+
+  config :archethic_fas,
+    api_port: System.get_env("ARCHETHIC_FAS_PORT", "3000") |> String.to_integer()
+end
